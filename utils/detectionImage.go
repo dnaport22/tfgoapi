c2621dbcb8140b95551d4b518ca83f71e33833f0
Nav Dhuti <navdhuti@AT-MacPro-005.local>
2018-08-08 15:20:54 +0100

    Adding new image normalisation method

--- utils/detectionImage.go
+++ utils/detectionImage.go
@@ -44,6 +44,40 @@ func (i *Img) GetImgType()string {
 	return i.ImgType
 }
 
+func ConstructGraphToNormaliseImage() (graph *tf.Graph, input, output tf.Output, err error) {
+	// Some constants specific to the pre-trained model at:
+	// https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip
+	//
+	// - The model was trained after with images scaled to 224x224 pixels.
+	// - The colors, represented as R, G, B in 1-byte each were converted to
+	//   float using (value - Mean)/Scale.
+	const (
+		H, W  = 224, 224
+		Mean  = float32(117)
+		Scale = float32(1)
+	)
+	// - input is a String-Tensor, where the string the JPEG-encoded image.
+	// - The inception model takes a 4D tensor of shape
+	//   [BatchSize, Height, Width, Colors=3], where each pixel is
+	//   represented as a triplet of floats
+	// - Apply normalization on each pixel and use ExpandDims to make
+	//   this single image be a "batch" of size 1 for ResizeBilinear.
+	s := op.NewScope()
+	input = op.Placeholder(s, tf.String)
+	output = op.Div(s,
+		op.Sub(s,
+			op.ResizeBilinear(s,
+				op.ExpandDims(s,
+					op.Cast(s,
+						op.DecodeJpeg(s, input, op.DecodeJpegChannels(3)), tf.Float),
+					op.Const(s.SubScope("make_batch"), int32(0))),
+				op.Const(s.SubScope("size"), []int32{H, W})),
+			op.Const(s.SubScope("mean"), Mean)),
+		op.Const(s.SubScope("scale"), Scale))
+	graph, err = s.Finalize()
+	return graph, input, output, err
+}
+
 func (i *Img) SetImgTensor() {
 	// DecodeJpeg uses a scalar String-valued tensor as input.
 	tensor, err := tf.NewTensor(string(i.ImageBytes))
@@ -51,7 +85,7 @@ func (i *Img) SetImgTensor() {
 		log.Fatal(err)
 	}
 	// Creates a tensorflow graph to decode the png Img
-	graph, input, output, err := decodePngGraph()
+	graph, input, output, err := ConstructGraphToNormaliseImage()
 	if err != nil {
 		log.Fatal(err)
 	}
