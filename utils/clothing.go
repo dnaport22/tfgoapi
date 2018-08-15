aeed30ee355235c5a2ca75d84053d52a1d49fede
Nav Dhuti <navdhuti@AT-MacPro-005.local>
2018-08-15 11:38:34 +0100

    Adding clothing rel tags

--- utils/clothing.go
+++ utils/clothing.go
@@ -1,8 +1,39 @@
 package utils
 
-func (c *AzureClothing) WhichClothing()string  {
-	if c.Western > c.Indian {
+var clothingObjects = []string{
+	"uniform",
+	"hat",
+	"suit",
+	"glasses",
+	"shirt",
+	"top",
+	"denim",
+	"skirt",
+	"traditional",
+}
+
+var colors = []string{
+	"white",
+	"black",
+	"red",
+	"green",
+	"blue",
+	"yellow",
+}
+
+func (c *Clothing) WhichClothing()string  {
+	if c.Group.Western > c.Group.Indian {
 		return "Western"
 	}
 	return "Indian"
+}
+
+func (c *Clothing) AssocTags()[]string {
+	var assoc []string
+	for _, v := range c.Assoc {
+		if contains(clothingObjects, v) {
+			assoc = append(assoc, v)
+		}
+	}
+	return assoc
 }
